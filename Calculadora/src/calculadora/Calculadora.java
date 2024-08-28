0ed75634a4466500928d342a8126d717662e12f0
Alvaro Rincon <alvarorinconc1@gmail.com>
2024-08-27 19:54:10 -0500

    Interfaz gráfica.
    - Se agregan los botones necesarios.
    - Se agregan los textField necesarios.
    - Se agregan los jLabel necesarios.
    - Se configuran los nombres de las variables.
    - Se eliminan comentarios innecesarios.

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,14 +1,12 @@
-
 package calculadora;
 
 import Vista.FormularioEntrada;
 
 public class Calculadora {
-
+    
     public static void main(String[] args) {
         
         FormularioEntrada verFormulario = new FormularioEntrada();
         verFormulario.setVisible(true);
     }
-    
 }
